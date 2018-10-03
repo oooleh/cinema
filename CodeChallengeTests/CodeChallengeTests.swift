3754c8664514907fc74ced2c3004ec57d3a2f662
oooleh <oooleh@gmail.com>
2018-10-03 23:12:39 +0200

    Added unit tests for movies list

--- CodeChallengeTests/CodeChallengeTests.swift
+++ CodeChallengeTests/CodeChallengeTests.swift
@@ -10,6 +10,15 @@ import XCTest
 
 class CodeChallengeTests: XCTestCase {
     
+    var moviesPages: [MoviesPage] {
+        let page1 = MoviesPage(page: 1, totalPages: 2, movies: [
+            Movie(id: 1, title: "title1", posterPath: "/1", overview: "overview1", releaseDate: nil),
+            Movie(id: 2, title: "title2", posterPath: "/2", overview: "overview2", releaseDate: nil)])
+        let page2 = MoviesPage(page: 2, totalPages: 2, movies: [
+            Movie(id: 3, title: "title3", posterPath: "/3", overview: "overview3", releaseDate: nil)])
+        return [page1, page2]
+    }
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,5 +27,26 @@ class CodeChallengeTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    // MARK: - MoviesList
+    
+    func testMoviesList_WhenHasMorePages() {
+        // given
+        let pages = moviesPages
+        // when
+        let vm = MoviesListViewModel(moviesPage: pages[0])
+        // then
+        XCTAssertTrue(vm.hasMorePages)
+    }
+    
+    func testMoviesList_WhenOnLastPage_ReturnHasNoMorePages() {
+        // given
+        let pages = moviesPages
+        // when
+        var vm = MoviesListViewModel(moviesPage: pages[0])
+        vm.appendPage(moviesPage: pages[1])
+        // then
+        XCTAssertFalse(vm.hasMorePages)
     }
 }
